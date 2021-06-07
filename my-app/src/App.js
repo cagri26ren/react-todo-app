b5493dcee4bd60d850e0d8411ff2e7ed66f593a5
Cagri Eren <cagri.eren@yahoo.co.uk>
2021-06-07 20:11:01 +0300

    Header implemented

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import './style/App.css';
 import MainTable from './components/MainTable.js'
+import Header from './components/Header.js'
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <MainTable></MainTable>
     </div>
   );
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import '../style/HeaderStyle.css'
+
+function Header() {
+  return (
+    <div id = "headerDivStyle">
+        <h1 id = "headerTextStyle">TODO List</h1>
+        <div id = "dividerStyle"></div>
+    </div>
+  );
+}
+
+export default Header;
