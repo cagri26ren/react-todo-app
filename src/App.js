55134023d876d6b805d98a7dde434da261ae3a6c
Cagri Eren <cagri.eren@yahoo.co.uk>
2021-06-11 14:56:11 +0300

    FooterMenu fixed according to airbnb linter

--- src/App.js
+++ src/App.js
@@ -5,54 +5,62 @@ import Header from './components/Header';
 import FooterMenu from './components/FooterMenu';
 
 function App() {
-  const [data,setData] = useState([]);
-  const [clickedArr,setClickedArr] = useState([]);
+  const [data, setData] = useState([]);
+  const [clickedArr, setClickedArr] = useState([]);
 
-  const handleCreate = useCallback(text => {
-    let arr = [...data];
-    arr.push({name:text, checked: false});
+  const handleCreate = useCallback((text) => {
+    const arr = [...data];
+    arr.push({ name: text, checked: false });
     setData(arr);
   });
 
-  const handleDelete = useCallback( index =>{
-  
-    let arr = [...data];
-    arr.splice(index,1);
+  const handleDelete = useCallback((index) => {
+    const arr = [...data];
+    arr.splice(index, 1);
     setData([...arr]);
-  },[data]);
+  }, [data]);
 
-  const handleCheckChange = useCallback( index =>{
-    let arr = [...data];
-    const prevName = arr[index]['name'];
-    const newChecked = !arr[index]['checked'];
-    arr[index] = {name:prevName, checked:newChecked}
+  const handleCheckChange = useCallback((index) => {
+    const arr = [...data];
+    const prevName = arr[index].name;
+    const newChecked = !arr[index].checked;
+    arr[index] = {name: prevName, checked: newChecked}
     setData(arr);
-  },[data]);
+  }, [data]);
 
-  const handleEditChange = useCallback( event =>{
-    let index = event.target.getAttribute('data-index');
-    let arr = [...data];
+  const handleEditChange = useCallback((event) => {
+    const index = event.target.getAttribute('data-index');
+    const arr = [...data];
     const newName = event.target.value;
-    arr[index] = {name:newName, checked:false}
+    arr[index] = { name: newName, checked: false };
     setData(arr);
-  },[data]);
+  }, [data]);
 
-  const handleRowClicked = useCallback( clickedIndex =>{
-    let map = [...clickedArr];
-    if( map[clickedIndex] === undefined ){
+  const handleRowClicked = useCallback((clickedIndex) => {
+    const map = [...clickedArr];
+    let found = false;
+
+    for (let i = 0; i < map.length; i += 1) {
+      if (map[i] === clickedIndex) {
+        found = true;
+      }
+    }
+
+    if (!found) {
       map.push(clickedIndex);
       setClickedArr(map);
     }
   });
 
-  const handleExitEditMode = useCallback( clickedIndex =>{
-    let map = [...clickedArr];
-    if( map[clickedIndex] !== undefined ){
-      map.splice(clickedIndex,1);
-      setClickedArr(map);
+  const handleExitEditMode = useCallback((clickedIndex) =>{
+    const map = [...clickedArr];
+    for (let i = 0; i < map.length; i += 1) {
+      if (map[i] === clickedIndex) {
+        map.splice(i, 1);
+        setClickedArr(map);
+      }
     }
   });
-
 
   return (
     <div>
--- src/components/FooterMenu.js
+++ src/components/FooterMenu.js
@@ -1,43 +1,51 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, React } from 'react';
+import PropTypes from 'prop-types';
 
-function FooterMenu(props) {
-    const[text,setText] = useState('');
+const FooterMenu = ({
+  handleCreate,
+  clickedArr,
+}) => {
+  const [text, setText] = useState('');
 
-    let textInput = null;
+  let textInput = null;
 
-    const handleSubmit = () =>{
-        props.handleCreate(text);
-    };  
+  const handleSubmit = () => {
+    handleCreate(text);
+  };
 
-    useEffect(() => {
-        console.log(props.clickedArr);
-        if( props.clickedArr.length === 0){
-            textInput.focus();
-        }
-        const onKeyDown = (e) => {
-            if (e.keyCode === 13) {
-                props.handleCreate(text);
-            }
-        }
+  useEffect(() => {
+    if (clickedArr.length === 0) {
+      textInput.focus();
+    }
+    const onKeyDown = (e) => {
+      if (e.keyCode === 13) {
+        handleCreate(text);
+      }
+    };
 
-        document.addEventListener('keydown', onKeyDown);
+    document.addEventListener('keydown', onKeyDown);
 
-        return () => {
-            document.removeEventListener('keydown', onKeyDown);
-        }
-    },[props,text]);
+    return () => {
+      document.removeEventListener('keydown', onKeyDown);
+    };
+  }, [clickedArr, handleCreate, text]);
 
-    return (
-        <div style = {{marginTop:'125px',marginLeft:'40%'}}>
-            <span>
-                <label>
-                    Task Name:
-                </label>
-                <input style={{marginRight:'10px'}} type="text" value={text} onChange = {(t) => setText(t.target.value)} ref={(text)=>{textInput=text}}/>
-                <button onClick={handleSubmit}  type="button">Create New Task</button>
-            </span>
-        </div>
-    );
-  }
-  
-  export default FooterMenu;
+  return (
+    <div style={{ marginTop: '125px', marginLeft: '40%' }}>
+      <span>
+        <label htmlFor="textInput">
+          Task Name:
+          <input name="textInput" style={{ marginRight: '10px' }} type="text" value={text} onChange={(t) => setText(t.target.value)} ref={(t) => { textInput = t; }} />
+        </label>
+        <button onClick={handleSubmit} type="button">Create New Task</button>
+      </span>
+    </div>
+  );
+};
+
+FooterMenu.propTypes = {
+  handleCreate: PropTypes.func.isRequired,
+  clickedArr: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
+
+export default FooterMenu;
