51a26bd09faa42c346547c571eb91887123868a8
Cagri Eren <cagri26.eren@gmail.com>
2021-06-15 13:56:57 +0300

    MainTable.js :: refactored according to redux

--- src/App.js
+++ src/App.js
@@ -5,94 +5,11 @@ import Header from './components/Header';
 import FooterMenu from './components/FooterMenu';
 
 function App() {
-  const [data, setData] = useState([]);
-  const [clickedArr, setClickedArr] = useState([]);
-
-  const handleCreate = useCallback((text) => {
-    const arr = [...data];
-    const newId = (data.length === 0) ? 0 : data[data.length - 1].id + 1;
-    arr.push({ id: newId, name: text, checked: false });
-    setData(arr);
-  }, [data]);
-
-  const handleDelete = useCallback((index) => {
-    const arr = [...data];
-    for (let i = 0; i < arr.length; i += 1) {
-      // eslint-disable-next-line eqeqeq
-      if (arr[i].id == index) {
-        arr.splice(i, 1);
-        setData(arr);
-      }
-    }
-  }, [data]);
-
-  const handleCheckChange = useCallback((index) => {
-    const arr = [...data];
-    for (let i = 0; i < arr.length; i += 1) {
-      // eslint-disable-next-line eqeqeq
-      if (arr[i].id == index) {
-        const prevName = arr[i].name;
-        const prevId = arr[i].id;
-        const newChecked = !arr[i].checked;
-        arr[i] = { id: prevId, name: prevName, checked: newChecked };
-        setData(arr);
-      }
-    }
-  }, [data]);
-
-  const handleEditChange = useCallback((event) => {
-    const index = event.target.getAttribute('data-index');
-    const arr = [...data];
-    for (let i = 0; i < arr.length; i += 1) {
-      // eslint-disable-next-line eqeqeq
-      if (arr[i].id == index) {
-        const prevId = data[i].id;
-        const newName = event.target.value;
-        arr[i] = { id: prevId, name: newName, checked: false };
-        setData(arr);
-      }
-    }
-  }, [data]);
-
-  const handleRowClicked = useCallback((clickedIndex) => {
-    const map = [...clickedArr];
-    let found = false;
-    for (let i = 0; i < map.length; i += 1) {
-      // eslint-disable-next-line eqeqeq
-      if (map[i] == clickedIndex) {
-        found = true;
-      }
-    }
-
-    if (!found) {
-      map.push(clickedIndex);
-      setClickedArr(map);
-    }
-  }, [clickedArr]);
-
-  const handleExitEditMode = useCallback((clickedIndex) => {
-    const map = [...clickedArr];
-    for (let i = 0; i < map.length; i += 1) {
-      // eslint-disable-next-line eqeqeq
-      if (map[i] == clickedIndex) {
-        map.splice(i, 1);
-        setClickedArr(map);
-      }
-    }
-  }, [clickedArr]);
-
   return (
     <div>
       <Header />
-      <MainTable
-        handleDelete={handleDelete}
-        handleCheckChange={handleCheckChange}
-        handleEditChange={handleEditChange}
-        data={data}
-        handleRowClicked={handleRowClicked}
-        handleExitEditMode={handleExitEditMode}
-      />
-      <FooterMenu handleCreate={handleCreate} clickedArr={clickedArr} />
+      <MainTable />
+      <FooterMenu />
     </div>
   );
 }
--- src/components/MainTable.js
+++ src/components/MainTable.js
@@ -1,39 +1,13 @@
 import '../style/MainTable.css';
 import '../style/App.css';
 import '../style/Header.css';
-import { useCallback, useMemo, React } from 'react';
-import PropTypes from 'prop-types';
+import { useMemo, React } from 'react';
+import { useSelector } from 'react-redux';
 import MainTableRow from './MainTableRow';
 
-const MainTable = ({
-  data,
-  handleDelete,
-  handleCheckChange,
-  handleEditChange,
-  handleExitEditMode,
-  handleRowClicked,
-}) => {
-  const mainTableSize = useMemo(() => 180 + 40 * data?.length, [data?.length]);
-
-  const onDeleteClick = useCallback((event) => {
-    handleDelete(event.target.getAttribute('data-index'));
-  }, [handleDelete]);
-
-  const onCheckChange = useCallback((event) => {
-    handleCheckChange(event.target.getAttribute('data-index'));
-  }, [handleCheckChange]);
-
-  const onEditChange = useCallback((event) => {
-    handleEditChange(event);
-  }, [handleEditChange]);
-
-  const onRowClick = useCallback((index) => {
-    handleRowClicked(index);
-  }, [handleRowClicked]);
-
-  const onClickExitEditMode = useCallback((index) => {
-    handleExitEditMode(index);
-  }, [handleExitEditMode]);
+const MainTable = () => {
+  const todoData = useSelector((state) => state.todo);
+  const mainTableSize = useMemo(() => 180 + 40 * todoData?.length, [todoData?.length]);
 
   return (
     <div className="mainTable" style={{ height: `${mainTableSize}px` }}>
@@ -51,34 +25,16 @@ const MainTable = ({
         </div>
         <div className="divider" />
       </div>
-      {data.map((element) => (
+      {todoData.map((element) => (
         <MainTableRow
           key={element?.id}
           name={element?.name}
           checked={element?.checked}
           dataIndex={element?.id}
-          onDeleteClick={onDeleteClick}
-          onCheckChange={onCheckChange}
-          onEditChange={onEditChange}
-          handleRowClicked={onRowClick}
-          handleExitEditMode={onClickExitEditMode}
         />
       ))}
     </div>
   );
-};
-
-MainTable.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number,
-    name: PropTypes.string,
-    checked: PropTypes.checked,
-  })).isRequired,
-  handleDelete: PropTypes.func.isRequired,
-  handleCheckChange: PropTypes.func.isRequired,
-  handleEditChange: PropTypes.func.isRequired,
-  handleExitEditMode: PropTypes.func.isRequired,
-  handleRowClicked: PropTypes.func.isRequired,
 };
 
 export default MainTable;
