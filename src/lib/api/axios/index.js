49aae66acc5bea5ecd74c6e1d1b9217daa8ced6a
Cagri Eren <cagri26.eren@gmail.com>
2021-06-19 14:17:55 +0300

    axios/index.js :: api updated according to restfull

--- src/lib/api/axios/index.js
+++ src/lib/api/axios/index.js
@@ -2,19 +2,25 @@ import axios from 'axios';
 
 const fetchTasks = () => axios.get('https://c-eren.jotform.dev/intern-api/v1/tasks');
 
-const addTask = (data) => axios.post(`https://c-eren.jotform.dev/intern-api/v1/task/${data.name}`);
-
-const editTaskName = (data) => axios({
-  method: 'put',
-  url: `https://c-eren.jotform.dev/intern-api/v1/task/name/${data.id}`,
+const addTask = (data) => axios({
+  method: 'post',
+  url: 'https://c-eren.jotform.dev/intern-api/v1/tasks/',
   data: {
     name: data.name,
   },
 });
 
-const editTaskCheck = (data) => axios.put(`https://c-eren.jotform.dev/intern-api/v1/task/check/${data.id}`);
+const editTaskName = (data) => axios({
+  method: 'put',
+  url: `https://c-eren.jotform.dev/intern-api/v1/tasks/name/${data.id}`,
+  data: {
+    name: data.name,
+  },
+});
 
-const deleteTask = (data) => axios.delete(`https://c-eren.jotform.dev/intern-api/v1/task/${data.id}`);
+const editTaskCheck = (data) => axios.put(`https://c-eren.jotform.dev/intern-api/v1/tasks/check/${data.id}`);
+
+const deleteTask = (data) => axios.delete(`https://c-eren.jotform.dev/intern-api/v1/tasks/${data.id}`);
 
 const api = {
   fetchTasks,
